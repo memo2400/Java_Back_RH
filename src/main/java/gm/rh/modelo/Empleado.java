5e3511b77f45f0ee4152ac27d2f467f1b6c39d54
Guillermo <memorosas.00@gmail.com>
2024-10-28 16:44:28 -0600

    se corrige el error de inici, no tenia mysql, se agregan mas cosas

---
+++ src/main/java/gm/rh/modelo/Empleado.java
@@ -0,0 +1,16 @@
+package gm.rh.modelo;
+
+import jakarta.persistence.Entity;      // con ello se crea la tabla bd en autmatico
+import lombok.AllArgsConstructor;
+import lombok.Data;                     // para crear los get y setters automarticos.
+import lombok.NoArgsConstructor;        // consturctor sin argumentos
+import lombok.ToString;
+
+// @Entity
+// @Data
+// @NoArgsConstructor
+// // @AllArgsConstructor
+// @ToString
+public class Empleado {
+
+}
