7d2c53ac5104182784bdd8e92739f08636c18458
Laptop <memorosas.00@gmail.com>
2024-10-28 22:48:29 -0600

    se prueba pero la tabla no se ha creado

--- src/main/java/gm/rh/modelo/Empleado.java
+++ src/main/java/gm/rh/modelo/Empleado.java
@@ -1,16 +1,26 @@
 package gm.rh.modelo;
 
 import jakarta.persistence.Entity;      // con ello se crea la tabla bd en autmatico
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;                     // para crear los get y setters automarticos.
 import lombok.NoArgsConstructor;        // consturctor sin argumentos
 import lombok.ToString;
 
-// @Entity
-// @Data
-// @NoArgsConstructor
-// // @AllArgsConstructor
-// @ToString
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // se genera en automatico la llave primaria.
+    
+    Integer idEmpleado;
+    String nombre;
+    String departamento;
+    Double sueldo;
 
 }
