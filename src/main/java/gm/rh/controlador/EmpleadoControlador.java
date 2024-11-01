6c75444a90953675b1ba8c6c7e395bfca6c1c6f8
Laptop <memorosas.00@gmail.com>
2024-11-01 17:36:48 -0600

    aplicacion funcional peticiones en navegador

---
+++ src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -0,0 +1,38 @@
+package gm.rh.controlador;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import gm.rh.modelo.Empleado;
+import gm.rh.servicios.EmpleadoServicio;
+import gm.rh.servicios.IEmpleadoServicio;
+
+@RestController             // co este recibimos y hacemos peticones http
+@RequestMapping("rh-app")             // como es a nivel clase se usa parentesis, seran localhost/rh-app
+@CrossOrigin("http://localhost:3000")            // esta recibira peticones del fron que esta en port 3000
+public class EmpleadoControlador {
+
+    private static final Logger logger =         // con esta enviaremos cosas a consola
+    LoggerFactory.getLogger(EmpleadoControlador.class);
+
+    @Autowired
+    private IEmpleadoServicio empleadoServicio;
+
+    //se agrega funcion para consultad todos loa empleados
+    @GetMapping("/empleados_maasivo")
+    public List<Empleado> obtenerEmpleados() {
+        var empleados = empleadoServicio.listarEmpleados();
+        // mandamos a imprimir el show
+        empleados.forEach((empleado -> logger.info(empleado.toString())));        // expresion lambda para procesar cada entrada
+
+        return empleados;
+
+    }
+}
